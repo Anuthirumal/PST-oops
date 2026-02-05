7a2d4a757a9cc4098233ef774da166f487372f3b
Anulakshmi T <anushanthi2005@gmail.com>
2026-02-05 10:27:34 +0530

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,20 @@
+package Program;
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		System.out.println(num());
+		System.out.println(sum());
+	}
+    public static int num() {
+	int a=56;
+    return a;
+	}
+	public static int sum() {
+		int a= 56;
+		int b= 40;
+		return a+b;
+	}
+	
+}
+ 
