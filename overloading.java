185641598f52556d22c74df600871be9ab85ffbe
Anulakshmi T <anushanthi2005@gmail.com>
2026-02-09 09:33:22 +0530

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,18 @@
+package Program;
+
+public class overloading {
+
+	public static void main(String[] args) {
+	 int a=10,b=20,c=30;
+	 System.out.println(add(a,b));
+	 System.out.println(add(a,b,c));
+
+	}
+	public static int add(int a, int b) {
+		return a+b;
+	}
+	public static int add(int a, int b, int c) {
+		return a+b+c;
+	}
+
+}
